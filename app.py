9525bafc50792be4e1229a74ad4a77fadfaf5fe8
jasman <jasman0603@gmail.com>
2025-12-31 12:11:00 +0700

    update streamlit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ def main():
     st.title("Dashboard COB")
     st.write("Hello Streamlit created by jasman!")
     st.write("This is a simple dashboard application application using Streamlit.")
+    st.write("Enjoy building your app!")
 
 if __name__ == "__main__":
     main()
