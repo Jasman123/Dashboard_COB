0d856db9174ff8bb09f3116cfd42ce25cf3a4902
jasman <jasman0603@gmail.com>
2025-12-31 11:58:34 +0700

    testing update dasboarhd code

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 def main():
     st.title("Dashboard COB")
     st.write("Hello Streamlit created by jasman!")
+    st.write("This is a simple dashboard application application using Streamlit.")
 
 if __name__ == "__main__":
     main()
